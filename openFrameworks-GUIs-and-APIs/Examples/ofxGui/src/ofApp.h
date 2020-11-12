93cd8aaf02ac8e92ebf0bae21f0712fdd57fcf3e
Tabitha Marcial <64868048+tabi-marc@users.noreply.github.com>
2020-11-12 20:08:27 +0400

    Initial commit

--- openFrameworks-GUIs-and-APIs/Examples/ofxGui/src/ofApp.h
+++ openFrameworks-GUIs-and-APIs/Examples/ofxGui/src/ofApp.h
@@ -1,1 +1,21 @@
-
+#pragma once
+#include "ofMain.h"
+#include "ofxGui.h"
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void playPressed();
+    void stopPressed();
+    void pausePressed();
+    
+    ofxPanel controller;
+    ofxIntSlider rValue, gValue, bValue, radius;
+    ofxFloatSlider xPos, yPos, vol;
+    ofxButton play, stop;
+    
+    ofSoundPlayer mySnd;
+};
