81c82b6b075e339cd4fd0586f3434c6e137cb698
Tabitha Marcial <64868048+tabi-marc@users.noreply.github.com>
2020-11-12 20:04:34 +0400

    Initial commit

--- openFrameworks-GUIs-and-APIs/Examples/classObjects/src/ofApp.h
+++ openFrameworks-GUIs-and-APIs/Examples/classObjects/src/ofApp.h
@@ -1,1 +1,17 @@
+#pragma once
 
+#include "ofMain.h"
+#include "Circle.hpp" //Provide access to our Circle.h in the ofApp.h
+#include <vector>
+
+class ofApp : public ofBaseApp{
+public:
+    void setup();
+    void update();
+    void draw();
+    void mousePressed(int x, int y, int button);
+    //create vector to store our circles
+    vector<Circle> circles;
+    //used to track circle count, and helps delete correct ones
+    int numCircles;
+};
