61de7c53230d29e58a2170d9acf35734d96360cf
Tabitha Marcial <64868048+tabi-marc@users.noreply.github.com>
2020-11-12 20:01:14 +0400

    Initial commit

--- openFrameworks-GUIs-and-APIs/Examples/betterMovement/src/ofApp.h
+++ openFrameworks-GUIs-and-APIs/Examples/betterMovement/src/ofApp.h
@@ -1,1 +1,23 @@
+#pragma once
 
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		
+    
+        //x and y positions for movement
+        float xPos, yPos;
+        //speed & velocity
+        float speed, velX, velY, friction, maxSpeed;
+        //boolean variables to check if key is pressed
+        bool uPressed, dPressed, lPressed, rPressed;
+		
+};
