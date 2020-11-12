38ae5321f50532a1234ac41052bee32eb59a24bb
Tabitha Marcial <64868048+tabi-marc@users.noreply.github.com>
2020-11-12 20:14:39 +0400

    Initial commit

--- openFrameworks-GUIs-and-APIs/Examples/timer/src/ofApp.cpp
+++ openFrameworks-GUIs-and-APIs/Examples/timer/src/ofApp.cpp
@@ -1,1 +1,28 @@
+#include "ofApp.h"
 
+void ofApp::setup(){
+    //set initial game time
+    seconds = 30;
+    //gives us time since app started
+    startTime = ofGetElapsedTimef();
+}
+
+void ofApp::update(){
+    //get time passed on each frame minus the start time
+    float tick = ofGetElapsedTimef() - startTime;
+    //if a second has passed reset startTime
+    if(tick > 1){
+        //set start time to time since app started
+        startTime = ofGetElapsedTimef();
+        //decrease remaining game time
+        seconds--;
+    }
+}
+
+void ofApp::draw(){
+    //clear string stream
+    gameInfo.str("");
+    //insert seconds into string stream
+    gameInfo << seconds << " seconds remaining" << endl;
+    ofDrawBitmapString(gameInfo.str(), 10, 10);
+}
