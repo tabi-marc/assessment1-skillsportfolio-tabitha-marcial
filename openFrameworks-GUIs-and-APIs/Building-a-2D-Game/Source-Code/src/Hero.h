44c0bf3ccf5a21bedadc1a24e7a9caa2f4a66c54
Tabitha Marcial <64868048+tabi-marc@users.noreply.github.com>
2020-11-12 19:45:21 +0400

    Initial commit

--- openFrameworks-GUIs-and-APIs/Building-a-2D-Game/Source-Code/src/Hero.h
+++ openFrameworks-GUIs-and-APIs/Building-a-2D-Game/Source-Code/src/Hero.h
@@ -1,1 +1,14 @@
+#pragma once
+#include "ofMain.h"
 
+class Hero {
+private:
+	enum directions { NONE, LEFT, RIGHT};//set of possible directions
+	directions dir;//variable to store direction
+public:
+	void setup();
+	void update();
+	void draw();
+	void setDirection(int key, bool pressed);//function to set position
+	int xPos, yPos;//store hero positions
+};
