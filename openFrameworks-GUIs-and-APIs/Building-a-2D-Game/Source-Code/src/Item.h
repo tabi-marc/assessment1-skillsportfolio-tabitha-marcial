b2222dcb9c4d584390743575b267e656ce7b233a
Tabitha Marcial <64868048+tabi-marc@users.noreply.github.com>
2020-11-12 19:46:59 +0400

    Initial commit

--- openFrameworks-GUIs-and-APIs/Building-a-2D-Game/Source-Code/src/Item.h
+++ openFrameworks-GUIs-and-APIs/Building-a-2D-Game/Source-Code/src/Item.h
@@ -1,1 +1,12 @@
+#pragma once
+#include "ofMain.h"
 
+class Item {
+private:
+	int r, g, b, size;//item variables
+public:
+	Item();//item constructor
+	void update();
+	void draw();
+	int xPos, yPos, points;//item variables
+};
