78c85c9460eb6b3c9c77223b5e43c247ca5d9180
Tabitha Marcial <64868048+tabi-marc@users.noreply.github.com>
2020-11-12 20:15:14 +0400

    Initial commit

--- openFrameworks-GUIs-and-APIs/Examples/timer/src/ofApp.h
+++ openFrameworks-GUIs-and-APIs/Examples/timer/src/ofApp.h
@@ -1,1 +1,15 @@
+#pragma once
 
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		float startTime;
+        int seconds;
+        stringstream gameInfo;
+};
