94183333073a4de962cb9b8624fe508c3408fd5d
Tabitha Marcial <64868048+marc-tabi@users.noreply.github.com>
2020-11-12 20:42:41 +0400

    Completed Exercise 1 - Biography

---
+++ Chapter-0-Getting-Started/Exercises/Ex1-Bio/Ex1-Bio/Ex1-Biography.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Declaration of variables.
+    string name = "Tabbie";
+    int age = 19;
+    string hometown = "Philippines";
+
+    //Prints my name, age, and hometown to the console.
+    cout << "Hello everyone, I am " << name << "." << endl << "\nMy age is " << age << " years old." << endl << "\nI came from the " << hometown << " which is my hometown.";
+
+    return 0;
+}
