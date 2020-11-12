c1d99099d20965e70f2a9c1cc3ace8a96ecd08bc
Tabitha Marcial <64868048+marc-tabi@users.noreply.github.com>
2020-11-12 20:54:35 +0400

    Completed Exercise 1 - Print

---
+++ Chapter-1-C++-Refresher/Exercises/Ex1-Print/Ex1-Print/Ex1-Print.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age = 10; //Declaration of the age variable.
+
+	//Prints the two outputs on the console window.
+	cout << " ____________________________________" << endl;
+	cout << "|                                    |" << endl;
+	cout << "| You are " << age << " years old.              |" << endl;
+	cout << "| You are too young to play the game!|" << endl;
+	cout << "|____________________________________|" << endl;
+
+	return 0;
+}
+
+
+
