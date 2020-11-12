7eceb04e88dbe9e5d663f0b3952289e4bd12a52e
Tabitha Marcial <64868048+tabi-marc@users.noreply.github.com>
2020-11-12 20:10:59 +0400

    Initial commit

--- openFrameworks-GUIs-and-APIs/Examples/simpleButtons/src/ofApp.h
+++ openFrameworks-GUIs-and-APIs/Examples/simpleButtons/src/ofApp.h
@@ -1,1 +1,12 @@
-
+#pragma once
+#include "ofMain.h"
+class ofApp : public ofBaseApp{
+public:
+	void setup();
+	void update();
+	void draw();
+	void mousePressed(int x, int y, int button);
+	
+	ofImage btnImg; //image object for button appearance
+	ofRectangle btn1, btn2; //rectangle object to hold buttons
+};
