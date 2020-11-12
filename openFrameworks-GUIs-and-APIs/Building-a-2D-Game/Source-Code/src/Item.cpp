c2e2bc82f0fb6880583403a85eba2d1acb92b0dd
Tabitha Marcial <64868048+tabi-marc@users.noreply.github.com>
2020-11-12 19:46:15 +0400

    Initial commit

--- openFrameworks-GUIs-and-APIs/Building-a-2D-Game/Source-Code/src/Item.cpp
+++ openFrameworks-GUIs-and-APIs/Building-a-2D-Game/Source-Code/src/Item.cpp
@@ -1,1 +1,19 @@
+#include "Item.h"
 
+Item::Item() {
+	yPos = -10;//set inital y
+	xPos = ofRandom(10, 1000); //set x to random value across screen
+	size = ofRandom(5, 15);//set size
+	points = 50 / size;//set points (smaller items worth more)
+	r = ofRandom(10, 255);//set r colour value
+	g = ofRandom(10, 255);//set g colour value
+	b = ofRandom(10, 255);//set b colour value
+}
+void Item::update() {
+	yPos += 5;//add to y value
+}
+
+void Item::draw() {
+	ofSetColor(r, g, b);//set colour based on rgb variables
+	ofDrawCircle(xPos, yPos, size);//draw circle on screen
+}
