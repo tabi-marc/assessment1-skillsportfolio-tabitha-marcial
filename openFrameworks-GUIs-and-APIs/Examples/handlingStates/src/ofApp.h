0e11cf3d084a5e12fb2ecda9894d54d3f02812a6
Tabitha Marcial <64868048+tabi-marc@users.noreply.github.com>
2020-11-12 20:06:28 +0400

    Initial commit

--- openFrameworks-GUIs-and-APIs/Examples/handlingStates/src/ofApp.h
+++ openFrameworks-GUIs-and-APIs/Examples/handlingStates/src/ofApp.h
@@ -1,1 +1,16 @@
+#pragma once
 
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		
+    char gameState;
+		
+};
