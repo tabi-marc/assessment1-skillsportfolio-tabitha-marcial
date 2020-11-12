7707c7e6cd6fbfe4916f97d26842d9b42dc34d3b
Tabitha Marcial <64868048+tabi-marc@users.noreply.github.com>
2020-11-12 20:02:52 +0400

    Initial commit

--- openFrameworks-GUIs-and-APIs/Examples/classObjects/src/Circle.hpp
+++ openFrameworks-GUIs-and-APIs/Examples/classObjects/src/Circle.hpp
@@ -1,1 +1,25 @@
+//
+//  Circle.hpp
+//  classObjects
+//
+//  Created by Jake Hobbs on 13/08/2020.
+//
 
+#ifndef Circle_hpp
+#define Circle_hpp
+
+#include "ofMain.h" //include core OF functionality
+class Circle{
+public:
+    //x and y position variable
+    float xPos, yPos;
+    //radius variable
+    int radius;
+
+    //setup with parameter arguments
+    void setup(float x, float y);
+    void update();
+    void draw();
+};
+
+#endif /* Circle_hpp */
