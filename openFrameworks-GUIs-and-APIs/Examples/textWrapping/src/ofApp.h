87f2beefa1136063222edbd156f79566c30220a4
Tabitha Marcial <64868048+tabi-marc@users.noreply.github.com>
2020-11-12 20:13:29 +0400

    Initial commit

--- openFrameworks-GUIs-and-APIs/Examples/textWrapping/src/ofApp.h
+++ openFrameworks-GUIs-and-APIs/Examples/textWrapping/src/ofApp.h
@@ -1,1 +1,17 @@
+#pragma once
 
+#include "ofMain.h"
+#include <string>//include string header
+#include <vector>//include vector header
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+    
+        string wrapString(string text, int width); //string wrapping function
+        ofTrueTypeFont myFont; //font object
+        string text; //string to hold text once wrapped
+};
